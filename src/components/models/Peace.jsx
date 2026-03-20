d8d8a7a7d3310cda757bc70a730a899d2d25d41f
sophienocode <sophieprotanki@gmail.com>
2026-03-19 23:33:28 -0300

    (Auth): Spatial audio that changes volume based on camera distance.
    React state control for play/pause.
    
    3D sound positioning linked to the cube's geometry.
    
    Interactivity where clicking the cube activates the audio

--- src/components/models/Peace.jsx
+++ src/components/models/Peace.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, PositionalAudio } from "@react-three/drei"
 import { useState } from "react";
 
 
@@ -11,6 +11,13 @@ function Peace() {
   return (
     <>
     <OrbitControls  />
+
+    {play && <PositionalAudio
+    url="./sound/peace.mp3"
+    autoplay 
+    loop
+    distance={5}
+    />}
 
       
     <mesh onClick={clickHandler}>
